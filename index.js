5aed7dcc53cb2e2a0788728eb4a434b541a01c70
Bryan C <159505672+ogprawnking@users.noreply.github.com>
2026-04-23 18:12:47 +0930

    JS functions calculate(), cleardisplay(), appendtodisplay() All have some function working. Can make calculations now!

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ function appendToDisplay(input) {
 }
 
 function clearDisplay() {
+  display.value = "";
 
 }
 
 function calculate() {
-
+  try{
+    display.value = eval(display.value);
+  }
+  catch(error){
+    display.value = "Error";
+  }
 }
